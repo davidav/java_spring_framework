455d96df8972f24b32618ddee83af50cf7061834
Andrey Davidenko <davidenkav@gmail.com>
2023-11-07 10:58:54 +0500

    init ListContacts app

---
+++ listcontacts/src/main/java/com/example/listcontacts/ListContactsApplication.java
@@ -0,0 +1,13 @@
+package com.example.listcontacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListContactsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListContactsApplication.class, args);
+	}
+
+}
