0fb1c27ec071a985a33b30683cdc30b5af2d12ac
andrei_davidenko <davidenkav@gmail.com>
2023-12-13 12:36:18 +0500

    lesson_6_init

---
+++ kafka-exemple-app/src/test/java/com/example/kafkaexempleapp/KafkaExempleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkaexempleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaExempleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
